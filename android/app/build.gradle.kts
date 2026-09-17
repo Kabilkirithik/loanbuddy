6f535ea51ad5b4a0987a5fcedded84edc8e01805
Kabilkirithik <kydykabil@gmail.com>
2026-09-17 12:04:04 +0530

    Upgrade geolocator to 14.0.0 and set compileSdk to 35 for AGP 8 compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.sitecheck"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
